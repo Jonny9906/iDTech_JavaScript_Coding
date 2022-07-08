cc12f5c9382a11035a2ea1954b8c77d12e2ea4a3
Jonathan Nguyen <jn33151@pausd.us>
2022-07-08 14:09:16 -0700

    GitHub Project

---
+++ Story Time/Story Time.js
@@ -0,0 +1,31 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = 'Welcome to my battleship!';
+let name = 'Spike';
+let title = 'King of Pirates';
+let battleShipName = 'Gale';
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 53;
+let year = 1437;
+let century = 15
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let items = ['cannon', 'sword', 'knive', ];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let favoriteItem = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+
+console.log(greeting + ' My name is ' + name + ', also known as the ' + title + '.');
+console.log('I am captain of the ' + battleShipName + 'battleship!');
+console.log('I am ' + age + ' years old and live in the year ' + year + ' which is the ' + century + "'th century!" + "\n");
+console.log('Questions?');
+console.log('Do you have a favorite every day item?');
+console.log(favoriteItem +  ', my favorite item to use is a ' + items[1]);
+console.log('Thank you for listening to my story! Feel free to visit my battleship whenever you want!');
---
+++ Working With Variables/Working With Variables.js
@@ -0,0 +1,18 @@
+/*Instructions
+ - Greet your friend by printing a message to the console.
+*/
+//your code here
+var myFriend = "Alex";
+
+function greetings() {
+  //also your code here
+  return("Greetings" + " " + myFriend + ".");
+  
+} 
+//leave this line unchanged to console log the results
+console.log('results: ', greetings());
+
+//don't change this line
+if (typeof module !== 'undefined') {
+  module.exports = greetings;
+}
